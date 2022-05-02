9e9b1c37697e4d417abf07d2a2068904ec69211e
surbhi-sharma1 <surbhi.sharma1@sourcefuse.com>
2022-05-02 12:12:18 +0530

    initial commit

---
+++ src/app/userModel.ts
@@ -0,0 +1,15 @@
+import Role from './enum'
+export class User {
+
+
+    id!: number
+    firstname!: string
+    middlename?: string;
+    lastname!: string
+    email!: string
+    phone!: string
+    role!: string
+    customer!: string
+    address!: string
+
+}
